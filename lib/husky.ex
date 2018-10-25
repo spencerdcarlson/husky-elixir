66a952ac151cd0dfc32dafdc3fc3d782386f705a
Spencer Carlson <spencerdcarlson@gmail.com>
2018-10-25 00:49:03 -0600

    comments

--- lib/husky.ex
+++ lib/husky.ex
@@ -1,5 +1,6 @@
 defmodule Husky do
   def main(argv \\ []) do
+    # TODO rename this file escript, have the mix task use halt appropriately and get the code form  System.cmd
     {stdout, _} = System.cmd("mix", ["husky.execute" | argv])
     {code, _} = stdout
     |> Integer.parse()
